70404a6f142f558f08679d77d24f12561bc22605
mlinxfeld <martin.linxfeld@foggykitchen.com>
2026-05-26 15:25:32 +0200

    Modernize function lesson2 for 2026

--- training/lesson2_custom_function/variables_UPDATED.tf
+++ training/lesson2_custom_function/variables_UPDATED.tf
@@ -1,6 +1,9 @@
 variable "tenancy_ocid" {}
+variable "user_ocid" {}
 variable "compartment_ocid" {}
 variable "region" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
 variable "ocir_user_name" {}
 variable "ocir_user_password" {}
 
